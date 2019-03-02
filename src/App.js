b5751e959e03bdf242d5ee692ad26b5f149bfe59
Porfirio Romo <porfirio.romo@yahoo.com.mx>
2019-03-02 01:58:54 -0600

    inicio Bulma

--- src/App.js
+++ src/App.js
@@ -2,24 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bulma/css/bulma.css'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="tiles is-ancestor">
+          <div className=" tile is-vertical is-8">
+            <div className="tile">
+              <div className="tile is-parent is-vertical">
+                <article className="tile is-child notification is-primary">Guía de viaje para Paco con </article>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
