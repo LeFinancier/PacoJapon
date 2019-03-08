dd69a6aa7ae5350ba83cc4cdb4f398488d47cd79
Porfirio Romo <porfirio.romo@yahoo.com.mx>
2019-03-07 22:26:48 -0600

    joss2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
+import {Button} from 'antd'
 
 class App extends Component {
   render() {
@@ -12,6 +12,7 @@ class App extends Component {
             <div className="tile">
               <div className="tile is-parent is-vertical">
                 <article className="tile is-child notification is-primary">Guía de viaje para Paco con Bulma</article>
+                <Button> sdfasds</Button>
               </div>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bulma/css/bulma.css'
+
+import 'antd/dist/antd.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 console.log('Hola')
